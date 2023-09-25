4d40b1be432ddd080120f709244af5f388576483
Chol Paul <cholthipaul@gmail.com>
2023-09-25 04:20:17 -0700

    completed bubble_sort.c

--- 0-bubble_sort.c
+++ 0-bubble_sort.c
@@ -27,20 +27,24 @@ void bubble_sort(int *array, size_t size)
 {
 	size_t i;
 	size_t j;
+	bool swapped;
 
 
-	if (array ==  NULL || size < 2)
+	if (array == NULL || size < 2)
 		return;
-
-	for (i = 0; i < size; i++)
+	for (i = 0; i < size - 1; i++)
 	{
+		swapped = false;
 		for (j = 0; j < size - i - 1; j++)
 		{
 			if (array[j] < array[j + 1])
 			{
-				swap_elements(array + j, array + j + 1);
+				swap_elements(&array[j], &array[j + 1]);
 				print_array(array, size);
+				swapped = true;
 			}
 		}
+		if (swapped  == false)
+			break;
 	}
 }
